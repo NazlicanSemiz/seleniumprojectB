000484515e3bfcc80fc4cdc1fbe175023c960292
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-04 22:34:53 +0000

    day2 Task5_getText getAttribute

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttiribute/homework1.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day2_locators_getText_getAttiribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class homework1 {
+    public static void main(String[] args) {
+
+        //TC #1: Etsy Title Verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to https://www.etsy.com
+        driver.get("https://www.etsy.com");
+
+        //3. Search for “wooden spoon”
+        WebElement searchBox = driver.findElement(By.name("apple"));
+        searchBox.sendKeys("apple" + Keys.ENTER);
+
+        //4. Verify title:
+        //Expected: “Wooden spoon | Etsy”
+        String expectedTitle = "Wooden spoon | Etsy";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.startsWith(expectedTitle)){
+            System.out.println("Title verification Passed!");
+        }else {
+            System.out.println("Title verification Failed!");
+        }
+
+    }
+}
