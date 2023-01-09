50036bcc03a89304f4d45a0cc38aa3723ad8ec55
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-09 21:06:13 +0000

    day5 TestNg intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TentNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TentNG_Intro.java
@@ -1,11 +1,30 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TentNG_Intro {
+
+    @BeforeMethod
+    public  void setUpMethod(){
+        System.out.println("--> BeforeMethod is running!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("--> AfterMethod is running!");
+    }
 
     @Test
     public void test1(){
         System.out.println("Test 1 is running...");
     }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running..");
+    }
+
+
 }
