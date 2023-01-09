c74aed08aa8e21fd49b2c4afb519bbeda0cb24e5
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-09 22:43:27 +0000

    day5 TestNg intro and selenium

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TentNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TentNG_Intro.java
@@ -14,11 +14,18 @@ public class TentNG_Intro {
         String expected= "apple";
 
         Assert.assertEquals(actual, expected);
+
     }
 
     @Test(priority = 2)
     public void test2(){
         System.out.println("Test 2 is running..");
+
+        //AssertTrue
+        String actual = "apple";
+        String expected= "apple";
+
+        Assert.assertTrue(actual.equals(expected));
     }
 
 
