6d2652dabad40583944570e1a9b05b0ed3d70f8c
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-12 23:13:53 +0000

    day Task1 Window Handling

--- src/test/java/com/cydeo/tests/day7_webtables_utilies_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilies_javafaker/T1_WindowHandling.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day7_webtables_utilies_javafaker;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -49,17 +50,15 @@ public class T1_WindowHandling {
 
 
         //5. Assert: Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        String expectedInTitle = "Etsy";
 
-        //Lines to be pasted:
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
-
-
+       // Assert.assertTrue(driver.getTitle().contains(expectedInTitle));
 
 
     }
-
-
-
 
 
 }
