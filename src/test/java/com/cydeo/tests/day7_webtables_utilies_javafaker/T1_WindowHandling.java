bb8ee7f5ed9896b26a5eb14d40e0a6871c34e096
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-12 23:00:48 +0000

    day Task1 Window Handling

--- src/test/java/com/cydeo/tests/day7_webtables_utilies_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilies_javafaker/T1_WindowHandling.java
@@ -1,21 +1,52 @@
 package com.cydeo.tests.day7_webtables_utilies_javafaker;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class T1_WindowHandling {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
     @Test
     public void window_handling_tes(){
 
-       // 2. Go to : https://www.amazon.com
-       // 3. Copy paste the lines from below into your class
-       //4. Create a logic to switch to the tab where Etsy.com is open
-       //5. Assert: Title contains “Etsy”
+        // 2. Go to : https://www.amazon.com
+        driver.get("https://www.amazon.com");
+
+        // 3. Copy paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+
+        //4. Create a logic to switch to the tab where Etsy.com is open
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+
+        }
+
+
+        //5. Assert: Title contains “Etsy”
 
         //Lines to be pasted:
-        //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
-        //((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
-       // ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+
 
 
 
