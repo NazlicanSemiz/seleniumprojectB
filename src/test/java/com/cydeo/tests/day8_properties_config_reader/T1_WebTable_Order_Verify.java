22715c25c3ef3c1e0e98f05d982da5f08fe4ef1f
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-13 23:23:31 +0000

    day8 T1_WebTable_Order_Verify

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -4,6 +4,7 @@ import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -28,17 +29,28 @@ public class T1_WebTable_Order_Verify {
         // Locate the cell that has Bob Martin text in it.
        WebElement bobMartinCell = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
 
-        // 7.satirdaki bob marlini ve 2. sutundaki cheese i secmek icin
+       System.out.println("bobMartinCell.getText() = " + bobMartinCell.getText());
+
+
+       // 7.satirdaki bob marlini ve 2. sutundaki cheese i secmek icin
        //table[@id='ctl00_MainContent_orderGrid']/tbody/tr[7]/td[2]
 
        //2. Verify Bob’s name is listed as expected.
-
-
        //Expected: “Bob Martin”
+       String expectedBobName= "Bob Martin";
+       String actualBobName= bobMartinCell.getText();
+
+       Assert.assertEquals(actualBobName,expectedBobName);
+
        //3. Verify Bob Martin’s order date is as expected
+
        //Expected: 12/31/2021
+       WebElement bobMartinDateCell =
+               driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']/following-sibling::td[3]"));
 
-
+       String expectedBobDate ="12/31/2021";
+       String actualBobDate= bobMartinDateCell.getText();
+       Assert.assertEquals(actualBobDate,expectedBobDate);
 
 
    }
