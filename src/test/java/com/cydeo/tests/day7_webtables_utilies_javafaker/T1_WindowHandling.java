f9ad0c905b9856f5d44db1128eea2f75560e68ca
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-13 04:01:49 +0000

    day7 Browser Utils  switchWindowAndVerify method

--- src/test/java/com/cydeo/tests/day7_webtables_utilies_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilies_javafaker/T1_WindowHandling.java
@@ -1,13 +1,12 @@
 package com.cydeo.tests.day7_webtables_utilies_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
@@ -34,31 +33,11 @@ public class T1_WindowHandling {
 
         //4. Create a logic to switch to the tab where Etsy.com is open
 
-        Set<String> allWindowsHandles = driver.getWindowHandles();
+        BrowserUtils.switchWindowAndVerify(driver, "etsy", "Etsy");
 
-        for (String each : allWindowsHandles) {
-
-            driver.switchTo().window(each);
-
-            System.out.println("Current URL: " + driver.getCurrentUrl());
-
-            if (driver.getCurrentUrl().contains("etsy")){
-                break;
 
             }
         }
 
 
-        //5. Assert: Title contains “Etsy”
-        String actualTitle = driver.getTitle();
-        String expectedInTitle = "Etsy";
 
-        Assert.assertTrue(actualTitle.contains(expectedInTitle));
-
-       // Assert.assertTrue(driver.getTitle().contains(expectedInTitle));
-
-
-    }
-
-
-}
