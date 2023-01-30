e4bc1aaf38d215df232a52ec7013e6083f500317
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-01-30 18:59:39 +0000

    day9 Java Faker, Test Base, Driver

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -5,6 +5,9 @@ In this class only general utility methods that are not related to some specific
  */
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
@@ -15,7 +18,7 @@ public class BrowserUtils {
     This method will accept int (in seconds) and execute Thread.sleep for given duration
      */
 
-    public void sleep(int second) {
+    public static void sleep(int second) {
         second *=1000;
         try {
             Thread.sleep(second);
@@ -65,10 +68,9 @@ This method accepts String "expectedTtitle" and Assert if its true
     /*
     Creating a utility method for ExplicitWait, so we don't have to repeat the lines
      */
-    /*
     public static void waitForInvisibilityOf(WebElement webElement){
-        //driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(driver.getDriver(), 10);
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
         wait.until(ExpectedConditions.invisibilityOf(webElement));
-    }*/
+    }
 }
